a0da9332037977908783764fd1ad4ceef106453c
rebzhaki <rebzhaki@gmail.com>
2024-05-24 18:39:44 +0300

    send tracking number via email

---
+++ Backend/src/mailer.ts
@@ -0,0 +1,46 @@
+import nodemailer from "nodemailer";
+import { resolve } from "path";
+
+interface EmailData {
+  complainant: { email: string; name: string }[];
+  TrackingNumber: string;
+}
+
+export const sendEmail = (data: EmailData): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    const { complainant, TrackingNumber } = data;
+    const [{ email, name }] = complainant;
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false,
+      auth: {
+        user: "maddison53@ethereal.email",
+        pass: "jn7jnAPss4f63QBp6D",
+      },
+    });
+
+    let content = {
+      from: '"Tiabidii Management" <tiabidiimanagement@official.sample>',
+      to: email,
+      subject: "Issue of tracking number",
+      text: `Hello ${name},
+Your tracking number for the task allocated is ${TrackingNumber}
+                  `,
+    };
+    let transportData = transporter.sendMail(content, (err, info) => {
+      if (err) {
+        reject(err);
+      } else {
+        return {
+          message: "email sent",
+          info: info.messageId,
+          preview: nodemailer.getTestMessageUrl(info),
+        };
+      }
+    });
+
+    resolve(transportData);
+  });
+};
